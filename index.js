00bb03e7bb0ae92ea900db0cbdb17da920a4b029
upsCRONOS <akronjonovelyorjon@gmail.com>
2024-03-15 23:51:43 +0500

    first commits

---
+++ index.js
@@ -0,0 +1,17 @@
+// const changeAddress = "1.1.1.1.1";
+
+// function ipAddress(ChangeAddress) {
+// 	let address = ChangeAddress.split(".").join("[.]");
+// 	console.log(address);
+
+// 	return address;
+// }
+// ipAddress(changeAddress);
+
+const toHome = "Men uyga ketyabman";
+function myHome(toHome) {
+	let goMyHome = toHome.lastIndexOf("ketyabman");
+	console.log(goMyHome);
+	return goMyHome;
+}
+myHome(toHome);
